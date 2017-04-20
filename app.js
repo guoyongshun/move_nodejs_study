d38c490096327756e9bd1e52b9f38f35a4dbc035
郭永顺 <a414644502@qq.com>
2017-04-20 01:24:09 -0700

    user login
    add user login with session

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ var _ = require('underscore');
 var Movie = require('./models/movie');
 var User = require('./models/user');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);
 var port = process.env.PORT || 3000;
 var app = express();
+var dbUrl = 'mongodb://localhost/imooc';
 
-mongoose.connect('mongodb://localhost/imooc');
+mongoose.connect(dbUrl);
 
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
@@ -17,12 +21,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.locals.moment = require('moment');
+app.use(cookieParser());
+app.use(session({
+  secret: 'imooc',
+  store: new mongoStore({
+    url: dbUrl,
+    collection: 'sessions'
+  })
+}));
 app.listen(port);
 
 console.log('started on port ' + port);
 
 // index page
 app.get('/', function(req, res){
+  console.log('user in session: ');
+  console.log(req.session.user);
   Movie.fetch(function(err, movies){
     if(err){
       console.log(err)
@@ -59,6 +73,38 @@ app.post('/user/signup', function(req, res){
     }
   })
 
+});
+
+
+//signin
+app.post('/user/signin', function(req, res){
+  var _user = req.body.user;
+  var name = _user.name;
+  var password = _user.password;
+
+  User.findOne({name: name}, function(err, user){
+    if(err){
+      console.log(err);
+    }
+
+    if(!user){
+      return res.redirect('/');
+    }
+
+    user.comparePassword(password, function(err, isMatch){
+      if(err){
+        console.log(err);
+      }
+
+      if(isMatch){
+        req.session.user = user;
+
+        return res.redirect('/');
+      }else{
+        console.log('Password is not matched');
+      }
+    })
+  });
 });
 
 
